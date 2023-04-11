a601a665eb3cd72fb0d3475984bb0344f8a9fdf4
Marvellous Odemwingie <modemwingie@appzonegroup.com>
2023-04-11 14:51:29 +0100

    fix for jwt

--- app/Interfaces/JwtServiceInterface.php
+++ app/Interfaces/JwtServiceInterface.php
@@ -3,10 +3,11 @@
 namespace App\Interfaces;
 
 use App\Models\User;
+use DateTimeImmutable;
 
 interface JwtServiceInterface
 {
     public function generateToken(User $user): string;
     public function parseToken(string $jwt): array;
-    public function validateToken(string $jwt): bool;
+    public function validateToken(string $jwt, DateTimeImmutable $exp): bool;
 }
--- app/Services/JwtService.php
+++ app/Services/JwtService.php
@@ -4,21 +4,26 @@ namespace App\Services;
 
 use App\Models\User;
 use App\Interfaces\JwtServiceInterface;
+use DateTimeImmutable;
+use Lcobucci\Clock\FrozenClock;
 use Lcobucci\JWT\Encoding\ChainedFormatter;
 use Lcobucci\JWT\Encoding\JoseEncoder;
 use Lcobucci\JWT\Signer\Rsa\Sha256;
 use Lcobucci\JWT\Signer\Key\InMemory;
 use Lcobucci\JWT\Token\Builder;
 use Lcobucci\JWT\Token\Parser;
+use Lcobucci\JWT\Validation\Constraint\IssuedBy;
+use Lcobucci\JWT\Validation\Constraint\SignedWith;
+use Lcobucci\JWT\Validation\Constraint\ValidAt;
 use Lcobucci\JWT\Validation\Validator;
 
 class JwtService implements JwtServiceInterface
 {
-    private Lcobucci\JWT\Signer\Key $privateKey;
+    private InMemory $privateKey;
     private string $issuer;
     private string $audience;
     private string $expiration;
-    private Lcobucci\JWT\Signer\Key $publicKey;
+    private InMemory $publicKey;
 
     public function __construct()
     {
@@ -26,7 +31,7 @@ class JwtService implements JwtServiceInterface
         $this->publicKey = InMemory::file(base_path('public.pem'));
         $this->issuer = config('jwt.issuer');
         $this->audience = config('jwt.audience');
-        $this->expiration = config('jwt.expiration');
+        $this->expiration = '+' . config('jwt.expiration') . ' seconds';
     }
 
     public function generateToken(User $user): string
@@ -38,7 +43,7 @@ class JwtService implements JwtServiceInterface
             ->issuedBy($this->issuer)
             ->permittedFor($this->audience)
             ->issuedAt($now)
-            ->expiresAt($now->modify($this->expiration . 's'))
+            ->expiresAt($now->modify($this->expiration))
             ->withClaim('uuid', $user->uuid)
             ->getToken($algorithm, $this->privateKey);
 
@@ -52,7 +57,7 @@ class JwtService implements JwtServiceInterface
         return $token->claims()->all();
     }
 
-    public function validateToken(string $jwt): bool
+    public function validateToken(string $jwt, DateTimeImmutable $exp): bool
     {
         $token = (new Parser(new JoseEncoder()))->parse($jwt);
 
@@ -60,8 +65,9 @@ class JwtService implements JwtServiceInterface
 
         return $validator->validate(
             $token,
-            new Lcobucci\JWT\Validation\Constraint\IssuedBy($this->issuer),
-            new Lcobucci\JWT\Validation\Constraint\SignedWith(new Sha256(), $this->publicKey)
+            new IssuedBy($this->issuer),
+            new SignedWith(new Sha256(), $this->publicKey),
+            new ValidAt(new FrozenClock($exp))
         );
     }
 }
