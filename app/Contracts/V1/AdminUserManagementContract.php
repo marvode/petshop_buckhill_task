a576bb35cd01b31bfac0b929ad639ec7e7146761
Marvellous Odemwingie <modemwingie@appzonegroup.com>
2023-04-14 00:20:05 +0100

    implemented admin user account management service

--- app/Contracts/V1/AdminUserManagementContract.php
+++ app/Contracts/V1/AdminUserManagementContract.php
@@ -4,8 +4,25 @@ namespace App\Contracts\V1;
 
 use App\DataTransferObjects\AdminUserEditDto;
 use App\Models\RegularUser;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 
 interface AdminUserManagementContract
 {
+
+    public function getAllUsers(
+        int $page,
+        int $limit,
+        string $sort,
+        bool $desc,
+        string $first_name,
+        string $last_name,
+        string $email,
+        string $phone_number,
+        bool|null $is_marketing,
+        string $created_at
+    ): LengthAwarePaginator;
+
     public function editUser(string $userUuid, AdminUserEditDto $details): RegularUser;
+
+    public function deleteUser(string $uuid): void;
 }
--- app/Services/V1/AdminUserManagementService.php
+++ app/Services/V1/AdminUserManagementService.php
@@ -5,6 +5,7 @@ namespace App\Services\V1;
 use App\Contracts\V1\AdminUserManagementContract;
 use App\DataTransferObjects\AdminUserEditDto;
 use App\Models\RegularUser;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Facades\Hash;
 
 class AdminUserManagementService implements AdminUserManagementContract
@@ -26,5 +27,34 @@ class AdminUserManagementService implements AdminUserManagementContract
         ]);
 
         return $user;
+    }
+
+    public function getAllUsers(int $page, int $limit, string $sort, bool $desc, string $first_name, string $last_name, string $email, string $phone_number, ?bool $is_marketing, string $created_at): LengthAwarePaginator
+    {
+        $query = RegularUser::query();
+
+        if ($first_name)
+            $query->where('first_name', 'like', "%$first_name%");
+        if ($last_name)
+            $query->where('last_name', 'like', "%$last_name%");
+        if ($email)
+            $query->where('email', 'like', "%$email%");
+        if ($phone_number)
+            $query->where('phone_number', 'like', "%$phone_number%");
+        if ($is_marketing)
+            $query->where('is_marketing', $is_marketing);
+        if ($created_at)
+            $query->whereDate('created_at', $created_at);
+
+        if ($desc)
+            return $query->orderByDesc($sort)->paginate($limit, ['*'], 'page', $page);
+        return $query->orderBy($sort)->paginate($limit, ['*'], 'page', $page);
+    }
+
+    public function deleteUser(string $uuid): void
+    {
+        $user = RegularUser::where('uuid', $uuid)->firstOrFail();
+
+        $user->delete();
     }
 }
